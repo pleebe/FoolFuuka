c756fbfbb373faac581391a393a96480730e9cce
pleebe <theplebmaster@gmail.com>
2017-03-11 19:19:34 +0000

    add theme head hook

--- assets/themes/foolz/foolfuuka-theme-foolfuuka/src/Layout/Chan.php
+++ assets/themes/foolz/foolfuuka-theme-foolfuuka/src/Layout/Chan.php
@@ -66,6 +66,7 @@ class Chan extends \Foolz\FoolFuuka\View\View
         <?php if ($radix->getValue('is_nsfw', false)) : ?><meta name="RATING" content="RTA-5042-1996-1400-1577-RTA"><?php endif; ?>
         <?php if ($radix->getValue('enable_math', false)) : ?><script src="<?= $this->getUri()->create('foolfuuka/mathjax/mathjax') ?>MathJax.js?config=default"></script><?php endif; ?>
     <?php endif; ?>
+    <?php \Foolz\Plugin\Hook::forge('foolfuuka.themes.all_header_code')->setObject($this)->execute(); ?>
     <?= $this->getPreferences()->get('foolframe.theme.header_code'); ?>
 
  </head>
